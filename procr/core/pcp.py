9a3f8561d84ecad92573777fc9cbd0b86791696a
Tyrn <orlov@bos-ltd.ru>
2019-04-11 09:29:26 +0300

    yapf applied

--- procr/core/pcp.py
+++ procr/core/pcp.py
@@ -47,21 +47,24 @@ def cmpstr_naturally(str_x, str_y):
     """
     num_x = str_strip_numbers(str_x)
     num_y = str_strip_numbers(str_y)
-    return cmpstr_c(num_x, num_y) if num_x != [] and num_y != [] else cmpstr_c(str_x, str_y)
+    return cmpstr_c(num_x, num_y) if num_x != [] and num_y != [] else cmpstr_c(
+        str_x, str_y)
 
 
 def compare_path(x, y):
     """
     Compares two paths (directories).
     """
-    return cmpstr_c(str(x), str(y)) if args.sort_lex else cmpstr_naturally(str(x), str(y))
+    return cmpstr_c(str(x), str(y)) if args.sort_lex else cmpstr_naturally(
+        str(x), str(y))
 
 
 def compare_file(x, y):
     """
     Compares two paths, filenames only, ignoring extensions.
     """
-    return cmpstr_c(x.stem, y.stem) if args.sort_lex else cmpstr_naturally(x.stem, y.stem)
+    return cmpstr_c(x.stem, y.stem) if args.sort_lex else cmpstr_naturally(
+        x.stem, y.stem)
 
 
 args = None
@@ -92,16 +95,21 @@ def list_dir_groom(abs_path, rev=False):
     offspring directory paths (1) naturally sorted list
     of offspring file paths.
     """
-    lst   = [abs_path.joinpath(x) for x in os.listdir(abs_path)]
-    dirs  = sorted([x for x in lst if x.is_dir()],
-                   key=ft.cmp_to_key((lambda xp, yp: -compare_path(xp, yp)) if rev else compare_path))
-    files = sorted([x for x in lst if isaudiofile(x)],
-                   key=ft.cmp_to_key((lambda xf, yf: -compare_file(xf, yf)) if rev else compare_file))
+    lst = [abs_path.joinpath(x) for x in os.listdir(abs_path)]
+    dirs = sorted(
+        [x for x in lst if x.is_dir()],
+        key=ft.cmp_to_key((
+            lambda xp, yp: -compare_path(xp, yp)) if rev else compare_path))
+    files = sorted(
+        [x for x in lst if isaudiofile(x)],
+        key=ft.cmp_to_key((
+            lambda xf, yf: -compare_file(xf, yf)) if rev else compare_file))
     return dirs, files
 
 
 def decorate_dir_name(i, path):
-    return ("" if args.strip_decorations else (str(i).zfill(3) + "-")) + path.name
+    return ("" if args.strip_decorations else
+            (str(i).zfill(3) + "-")) + path.name
 
 
 def artist():
@@ -118,8 +126,10 @@ def decorate_file_name(cntw, i, dst_step, path):
 
     if args.strip_decorations: return path.name
     prefix = (str(i).zfill(cntw) +
-             ('-' + '-'.join(dst_step) + '-' if args.prepend_subdir_name and not args.tree_dst and len(dst_step) else '-'))
-    return prefix + (args.unified_name + " - " + artist() + path.suffix if args.unified_name else path.name)
+              ('-' + '-'.join(dst_step) + '-' if args.prepend_subdir_name
+               and not args.tree_dst and len(dst_step) else '-'))
+    return prefix + (args.unified_name + " - " + artist() + path.suffix
+                     if args.unified_name else path.name)
 
 
 def traverse_tree_dst(src_dir, dst_root, dst_step, cntw):
@@ -136,7 +146,8 @@ def traverse_tree_dst(src_dir, dst_root, dst_step, cntw):
         yield from traverse_tree_dst(d, dst_root, step, cntw)
 
     for i, f in enumerate(files):
-        dst_path = dst_root.joinpath(*dst_step).joinpath(decorate_file_name(cntw, i, dst_step, f))
+        dst_path = dst_root.joinpath(*dst_step).joinpath(
+            decorate_file_name(cntw, i, dst_step, f))
         yield f, dst_path
 
 
@@ -153,7 +164,8 @@ def traverse_flat_dst(src_dir, dst_root, fcount, dst_step, cntw):
         yield from traverse_flat_dst(d, dst_root, fcount, step, cntw)
 
     for f in files:
-        dst_path = dst_root.joinpath(decorate_file_name(cntw, fcount[0], dst_step, f))
+        dst_path = dst_root.joinpath(
+            decorate_file_name(cntw, fcount[0], dst_step, f))
         fcount[0] += 1
         yield f, dst_path
 
@@ -166,7 +178,8 @@ def traverse_flat_dst_r(src_dir, dst_root, fcount, dst_step, cntw):
     dirs, files = list_dir_groom(src_dir, rev=True)
 
     for f in files:
-        dst_path = dst_root.joinpath(decorate_file_name(cntw, fcount[0], dst_step, f))
+        dst_path = dst_root.joinpath(
+            decorate_file_name(cntw, fcount[0], dst_step, f))
         fcount[0] -= 1
         yield f, dst_path
 
@@ -181,7 +194,7 @@ def groom(src, dst, cnt):
     Makes an 'executive' run of traversing the source directory; returns the 'ammo belt' generator.
     """
     global args
-    cntw = len(str(cnt))      # File number substring need not be wider than this
+    cntw = len(str(cnt))  # File number substring need not be wider than this
 
     if args.tree_dst:
         return traverse_tree_dst(src, dst, [], cntw)
@@ -200,7 +213,8 @@ def build_album():
     global args
 
     prefix = (str(args.album_num).zfill(2) + "-") if args.album_num else ""
-    base_dst = prefix + (artist() + " - " + args.unified_name if args.unified_name else args.src_dir.name)
+    base_dst = prefix + (artist() + " - " + args.unified_name
+                         if args.unified_name else args.src_dir.name)
 
     executive_dst = args.dst_dir.joinpath("" if args.drop_dst else base_dst)
 
@@ -219,7 +233,9 @@ def build_album():
     tot = audiofiles_count(args.src_dir)
 
     if tot < 1:
-        print(f'There are no supported audio files in the source directory "{args.src_dir}".')
+        print(
+            f'There are no supported audio files in the source directory "{args.src_dir}".'
+        )
         sys.exit()
 
     if not args.drop_dst:
@@ -236,8 +252,10 @@ def make_initials(authors, sep=".", trail=".", hyph="-"):
     """
     Reduces authors to initials.
     """
-    by_space = lambda s: sep.join(x[0] for x in re.split(f"[\s{sep}]+", s) if x).upper()
-    by_hyph  = lambda s: hyph.join(by_space(x) for x in re.split(f"\s*(?:{hyph}\s*)+", s)) + trail
+    by_space = lambda s: sep.join(x[0] for x in re.split(f"[\s{sep}]+", s) if x
+                                  ).upper()
+    by_hyph = lambda s: hyph.join(
+        by_space(x) for x in re.split(f"\s*(?:{hyph}\s*)+", s)) + trail
 
     sans_monikers = re.sub(r"\"(?:\\.|[^\"\\])*\"", " ", authors)
 
@@ -251,7 +269,6 @@ def copy_album():
     global args
 
     def _set_tags(i, total, source, path):
-
         def _title(s):
             if args.file_title_num:
                 return str(i) + '>' + source.stem
@@ -266,16 +283,16 @@ def copy_album():
         if not args.drop_tracknumber:
             audio["tracknumber"] = str(i) + "/" + str(total)
         if args.artist_tag and args.album_tag:
-            audio["title"]       = _title(make_initials(args.artist_tag) +
-                                          " - " + args.album_tag)
-            audio["artist"]      = args.artist_tag
-            audio["album"]       = args.album_tag
+            audio["title"] = _title(
+                make_initials(args.artist_tag) + " - " + args.album_tag)
+            audio["artist"] = args.artist_tag
+            audio["album"] = args.album_tag
         elif args.artist_tag:
-            audio["title"]       = _title(args.artist_tag)
-            audio["artist"]      = args.artist_tag
+            audio["title"] = _title(args.artist_tag)
+            audio["artist"] = args.artist_tag
         elif args.album_tag:
-            audio["title"]       = _title(args.album_tag)
-            audio["album"]       = args.album_tag
+            audio["title"] = _title(args.album_tag)
+            audio["album"] = args.album_tag
         audio.save()
 
     def _cp(i, total, entry):
@@ -314,40 +331,77 @@ def retrieve_args():
     or in the reversed order. This can be important for some mobile devices.
     ''')
 
-    parser.add_argument("-v", "--verbose", help="verbose output",
-                        action="store_true")
-    parser.add_argument("-d", "--drop-tracknumber", help="do not set track numbers",
-                        action="store_true")
-    parser.add_argument("-s", "--strip-decorations", help="strip file and directory name decorations",
-                        action="store_true")
-    parser.add_argument("-f", "--file-title", help="use file name for title tag",
-                        action="store_true")
-    parser.add_argument("-F", "--file-title-num", help="use numbered file name for title tag",
-                        action="store_true")
-    parser.add_argument("-x", "--sort-lex", help="sort files lexicographically",
-                        action="store_true")
-    parser.add_argument("-t", "--tree-dst", help="retain the tree structure of the source album at destination",
-                        action="store_true")
-    parser.add_argument("-p", "--drop-dst", help="do not create destination directory",
-                        action="store_true")
-    parser.add_argument("-r", "--reverse", help="copy files in reverse order (number one file is the last to be copied)",
-                        action="store_true")
-    parser.add_argument("-e", "--file-type", help="accept only audio files of the specified type")
-    parser.add_argument("-i", "--prepend-subdir-name", help="prepend current subdirectory name to a file name",
-                        action="store_true")
-    parser.add_argument("-u", "--unified-name",
-                        help='''
+    parser.add_argument(
+        "-v", "--verbose", help="verbose output", action="store_true")
+    parser.add_argument(
+        "-d",
+        "--drop-tracknumber",
+        help="do not set track numbers",
+        action="store_true")
+    parser.add_argument(
+        "-s",
+        "--strip-decorations",
+        help="strip file and directory name decorations",
+        action="store_true")
+    parser.add_argument(
+        "-f",
+        "--file-title",
+        help="use file name for title tag",
+        action="store_true")
+    parser.add_argument(
+        "-F",
+        "--file-title-num",
+        help="use numbered file name for title tag",
+        action="store_true")
+    parser.add_argument(
+        "-x",
+        "--sort-lex",
+        help="sort files lexicographically",
+        action="store_true")
+    parser.add_argument(
+        "-t",
+        "--tree-dst",
+        help="retain the tree structure of the source album at destination",
+        action="store_true")
+    parser.add_argument(
+        "-p",
+        "--drop-dst",
+        help="do not create destination directory",
+        action="store_true")
+    parser.add_argument(
+        "-r",
+        "--reverse",
+        help=
+        "copy files in reverse order (number one file is the last to be copied)",
+        action="store_true")
+    parser.add_argument(
+        "-e",
+        "--file-type",
+        help="accept only audio files of the specified type")
+    parser.add_argument(
+        "-i",
+        "--prepend-subdir-name",
+        help="prepend current subdirectory name to a file name",
+        action="store_true")
+    parser.add_argument(
+        "-u",
+        "--unified-name",
+        help='''
                         destination root directory name and file names are based on UNIFIED_NAME,
                         serial number prepended, file extensions retained; also album tag,
                         if the latter is not specified explicitly
                         ''')
-    parser.add_argument("-b", "--album-num", help="0..99; prepend ALBUM_NUM to the destination root directory name")
+    parser.add_argument(
+        "-b",
+        "--album-num",
+        help="0..99; prepend ALBUM_NUM to the destination root directory name")
     parser.add_argument("-a", "--artist-tag", help="artist tag name")
     parser.add_argument("-g", "--album-tag", help="album tag name")
     parser.add_argument('src_dir', help="source directory")
     parser.add_argument('dst_dir', help="general destination directory")
     rg = parser.parse_args()
-    rg.src_dir = Path(rg.src_dir).absolute()    # Takes care of the trailing slash, too.
+    rg.src_dir = Path(
+        rg.src_dir).absolute()  # Takes care of the trailing slash, too.
     rg.dst_dir = Path(rg.dst_dir).absolute()
 
     if not rg.src_dir.is_dir():
--- tests/test_common.py
+++ tests/test_common.py
@@ -3,7 +3,6 @@ from procr.core.pcp import *
 
 
 class TestHelpers(unittest.TestCase):
-
     def setUp(self):
         pass
 
@@ -15,9 +14,12 @@ class TestHelpers(unittest.TestCase):
         self.assertEqual(str(15331).zfill(3), "15331")
 
     def test_has_ext_of(self):
-        self.assertEqual(has_ext_of(Path("/alfa/bra.vo/charlie.ogg"), "OGG"), True)
-        self.assertEqual(has_ext_of(Path("/alfa/bra.vo/charlie.ogg"), ".ogg"), True)
-        self.assertEqual(has_ext_of(Path("/alfa/bra.vo/charlie.ogg"), "mp3"), False)
+        self.assertEqual(
+            has_ext_of(Path("/alfa/bra.vo/charlie.ogg"), "OGG"), True)
+        self.assertEqual(
+            has_ext_of(Path("/alfa/bra.vo/charlie.ogg"), ".ogg"), True)
+        self.assertEqual(
+            has_ext_of(Path("/alfa/bra.vo/charlie.ogg"), "mp3"), False)
 
     def test_str_strip_numbers(self):
         self.assertEqual(str_strip_numbers("ab11cdd2k.144"), [11, 2, 144])
@@ -41,14 +43,21 @@ class TestHelpers(unittest.TestCase):
 
     def test_make_initials(self):
         self.assertEqual(make_initials(" "), ".")
-        self.assertEqual(make_initials("John ronald reuel Tolkien"), "J.R.R.T.")
+        self.assertEqual(
+            make_initials("John ronald reuel Tolkien"), "J.R.R.T.")
         self.assertEqual(make_initials("  e.B.Sledge "), "E.B.S.")
         self.assertEqual(make_initials("Apsley Cherry-Garrard"), "A.C-G.")
-        self.assertEqual(make_initials("Windsor Saxe-\tCoburg - Gotha"), "W.S-C-G.")
+        self.assertEqual(
+            make_initials("Windsor Saxe-\tCoburg - Gotha"), "W.S-C-G.")
         self.assertEqual(make_initials("Elisabeth Kubler-- - Ross"), "E.K-R.")
-        self.assertEqual(make_initials("  Fitz-Simmons Ashton-Burke Leigh"), "F-S.A-B.L.")
+        self.assertEqual(
+            make_initials("  Fitz-Simmons Ashton-Burke Leigh"), "F-S.A-B.L.")
         self.assertEqual(make_initials('Arleigh "31-knot"Burke '), "A.B.")
-        self.assertEqual(make_initials('Harry "Bing" Crosby, Kris "Tanto" Paronto'), "H.C.,K.P.")
+        self.assertEqual(
+            make_initials('Harry "Bing" Crosby, Kris "Tanto" Paronto'),
+            "H.C.,K.P.")
         self.assertEqual(make_initials("a.s.,b.s."), "A.S.,B.S.")
-        self.assertEqual(make_initials("A. Strugatsky, B...Strugatsky."), "A.S.,B.S.")
-        self.assertEqual(make_initials("Иржи Кропачек, Йозеф Новотный"), "И.К.,Й.Н.")
+        self.assertEqual(
+            make_initials("A. Strugatsky, B...Strugatsky."), "A.S.,B.S.")
+        self.assertEqual(
+            make_initials("Иржи Кропачек, Йозеф Новотный"), "И.К.,Й.Н.")
